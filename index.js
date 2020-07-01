a86b8b27b29cf514b20b8b01faf954e5da1fa8ac
Momijiichigo <ichigomomiji436@gmail.com>
2020-07-01 19:03:23 +0900

    test

--- index.js
+++ index.js
@@ -8,20 +8,45 @@ const init = (el)=>{
   el.addEventListener("mousemove", handleMove, false);
   
 }
+/**
+ * 
+ * @var {number} VTI Valid Time Interval for recognizing multi times tapping
+ */
+let VTI = 400;
+/**
+ * @var {number} TC tap count
+ */
+let TC = 0;
+/**
+ * @var {object} rezClear reservation for clearing TC
+ */
+let rezClear;
 const triggerList = [];
+/*
+{
+  gestureList: [],
+
+}
+whenDoneWithGesture,
+whileSliding,
+cancelSliding
+*/ 
 const addTrigger = () =>{
 
 }
 
 //stopPropagation
 const handleStart = e =>{
-
+  rezClear.clearTimeout && rezClear.clearTimeout();
+  TC++;
 }
 const handleMove = e =>{
 
 }
 const handleEnd = e =>{
-
+  rezClear = setTimeout(()=>{
+    TC = 0;
+  },VTI)
 }
 
 const handleCancel = e =>{
@@ -29,6 +54,11 @@ const handleCancel = e =>{
 }
 
 GestureTrigger = {
+  /**
+   * 
+   * @property {number} VTI Valid Time Interval for recognizing multi times tapping
+   */
   init: init,
   addTrigger: addTrigger,
+  VTI: VTI
 }
